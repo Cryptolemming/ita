c2a17c0a9a6c3cd5ed2e4c268a29ad5a14d1943c
cryptolemming <aliayoub22@gmail.com>
2019-11-27 16:42:13 -0500

    modify: maxSubArray for full division, all negatives

--- ch4/exercises.js
+++ ch4/exercises.js
@@ -1,36 +1,38 @@
 const maxSubArray = A => {
-  return maxSubArrayHelper(A, 0, A.length-1)
+  const result = maxSubArrayHelper(A, 0, A.length-1);
+  return A.slice(result.l, result.r+1)
 }
 
 const maxSubArrayHelper = (A, low, high) => {
-  if (low === high) { return { l: low, r: high, maxSum: A[low] }; }
+  if (low === high) { return { l: low, r: high, maxSum: A[low] } }
   else {
     const mid = Math.floor((low+high) / 2);
+    const { l: subLLeft, r: subLRight, maxSum: subLSum } = maxSubArrayHelper(A, low, mid);
+    const { l: subRLeft, r: subRRight, maxSum: subRSum } = maxSubArrayHelper(A, mid+1, high);
     const { l: leftLow, r: leftHigh, maxSum: leftSum } = maxContiguousSubArray(A, low, mid);
     const { l: rightLow, r: rightHigh, maxSum: rightSum } = maxContiguousSubArray(A, mid+1, high);
     const { l: crossLeft, r: crossRight, maxSum: crossSum } = maxCrossingSubArray(A, low, mid, high);
-    if (leftSum >= rightSum && leftSum >= crossSum) {
-      return { l: leftLow, r: leftHigh, maxSum: leftSum }
-    } else if (rightSum >= leftSum && rightSum >= crossSum) {
-      return { l: rightLow, r: rightHigh, maxSum: rightSum }
-    } else {
-      return { l: crossLeft, r: crossRight, maxSum: crossSum }
-    }
+
+    const max = Math.max(leftSum, rightSum, crossSum, subLSum, subRSum)
+    if (max === leftSum) { return { l: leftLow, r: leftHigh, maxSum: leftSum }; }
+    if (max === rightSum) { return { l: rightLow, r: rightHigh, maxSum: rightSum }; }
+    if (max === crossSum) { return { l: crossLeft, r: crossRight, maxSum: crossSum }; }
+    if (max === subLSum) { return { l: subLLeft, r: subLRight, maxSum: subLSum }; }
+    if (max === subRSum) { return { l: subRLeft, r: subRRight, maxSum: subRSum }; }
   }
 }
 
 const maxContiguousSubArray = (A, l, r) => {
-  const { l: newLeft, r: newRight, maxSum } = A.reduce((acc, num, idx) => {
-    acc.sum += num;
-    if (acc.sum > acc.maxSum) {
-      acc.maxSum = acc.sum;
-      acc.r = acc.l + idx;
+  let sum = 0;
+  let maxSum = Number.MIN_SAFE_INTEGER;
+  let max = l;
+  for (let i = l; i <= r; i++) {
+    sum += A[i];
+    if (sum > maxSum) {
+      maxSum = sum;
     }
-
-    return acc;
-  }, { l, r: l, sum: 0, maxSum: Number.MIN_SAFE_INTEGER })
-
-  return { l: newLeft, r: newRight, maxSum }
+  }
+  return { l: l, r: max, maxSum}
 }
 
 const maxCrossingSubArray = (A, l, m, r) => {
@@ -55,7 +57,9 @@ const maxCrossingSubArray = (A, l, m, r) => {
       maxRight = i;
     }
   }
-  return { l: maxLeft, r: maxRight, maxSum: maxLeft + maxRight}
+
+  return { l: maxLeft, r: maxRight, maxSum: leftSum + rightSum }
 }
 
 console.log(maxSubArray([3,-2,4,-4]))
+console.log(maxSubArray([-3,-2,-4,-4]))
