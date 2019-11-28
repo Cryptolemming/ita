475449910110810a9ad4ccacedcccf56c7f24f37
cryptolemming <aliayoub22@gmail.com>
2019-11-28 00:17:44 -0500

    add: measures and comparison to brute force for runtime crossover

--- ch4/exercises.js
+++ ch4/exercises.js
@@ -1,5 +1,32 @@
-const maxSubArray = A => {
-  const result = maxSubArrayHelper(A, 0, A.length-1);
+let operations = 0;
+
+const maxSubArray = (A, type) => {
+  if (type === 'brute') {
+    return maxSubArrayBrute(A)
+  } else {
+    const result = maxSubArrayHelper(A, 0, A.length-1);
+    return A.slice(result.l, result.r+1)
+  }
+}
+
+const maxSubArrayBrute = A => {
+  const len = A.length;
+  const result = A.reduce((acc, num, idx) => {
+    let i = idx;
+    while (i < len) {
+      operations++;
+      acc.sum = num;
+      if (acc.sum > acc.maxSum) {
+        acc.maxSum = acc.sum;
+        acc.l = idx;
+        acc.r = i;
+      }
+      i++;
+    }
+
+    return acc;
+  }, { l: 0, r: 0, sum: 0, maxSum: Number.MIN_SAFE_INTEGER });
+
   return A.slice(result.l, result.r+1)
 }
 
@@ -27,6 +54,7 @@ const maxContiguousSubArray = (A, l, r) => {
   let maxSum = Number.MIN_SAFE_INTEGER;
   let max = l;
   for (let i = l; i <= r; i++) {
+    operations++;
     sum += A[i];
     if (sum > maxSum) {
       maxSum = sum;
@@ -40,6 +68,7 @@ const maxCrossingSubArray = (A, l, m, r) => {
   let sum = 0;
   let maxLeft = m;
   for (let i = m; i >= l; i--) {
+    operations++;
     sum += A[i];
     if (sum > leftSum) {
       leftSum = sum;
@@ -51,6 +80,7 @@ const maxCrossingSubArray = (A, l, m, r) => {
   sum = 0;
   let maxRight = m;
   for (let i = m+1; i <= r; i++) {
+    operations++;
     sum += A[i];
     if (sum > rightSum) {
       rightSum = sum;
@@ -61,5 +91,41 @@ const maxCrossingSubArray = (A, l, m, r) => {
   return { l: maxLeft, r: maxRight, maxSum: leftSum + rightSum }
 }
 
-console.log(maxSubArray([3,-2,4,-4]))
-console.log(maxSubArray([-3,-2,-4,-4]))
+console.log('brute force:\n')
+let sample = 1;
+while (sample <= 20) {
+  // brute force
+  let operationsAtStart = operations;
+
+  let start = process.hrtime()
+  maxSubArray([...Array(sample).keys()], 'brute')
+  let end = process.hrtime(start)
+
+  console.log(`BF - sample: ${sample}, time: ${end[1] / 1000000}ms, operations: ${operations-operationsAtStart}`)
+
+  // dnc
+  operationsAtStart = operations;
+
+  start = process.hrtime()
+  maxSubArray([...Array(sample).keys()], 'dnc')
+  end = process.hrtime(start)
+
+  console.log(`DNC - sample: ${sample}, time: ${end[1] / 1000000}ms, operations: ${operations-operationsAtStart}\n`)
+  sample++;
+}
+
+/*
+  ### operations faster in DNC at n = 15 according to implementation
+
+  # DNC - nlogn (contiguous = 1, crossing = 1) = ~ (n*2) * logn operations
+    n = 15 = (15*2) * (4) = ~ 120 operations
+  # BF - (n(n-1)) / 2 = (n^2 - n) / 2
+    n = 15 = (15^2 - 15) / 2 = 105 operations
+
+  ### expected at n = 20
+
+  # DNC
+    (20*2) * 4.5 = 180 operations
+  # BF
+    (20^2 - 20) / 2 = 190 operations
+*/
